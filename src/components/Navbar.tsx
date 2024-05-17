240c12d3c1fee3284c3ca4ad3efb8ce839f27cf3
Rohit Paswan <paswanrohit0207@gmail.com>
2024-05-17 12:07:37 +0530

    first commit - Basic setup for app

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IoMenu } from "react-icons/io5";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoIosCloseCircle } from "react-icons/io";
+import { FaSearch } from "react-icons/fa"
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
